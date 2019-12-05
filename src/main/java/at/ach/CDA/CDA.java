330db871b2e5414df736b3d74fda356bdf998414
Ingo Weigel <ingo.weigel@myoha.at>
2019-12-05 16:39:46 +0100

    another test commit

--- src/main/java/at/ach/CDA/CDA.java
+++ src/main/java/at/ach/CDA/CDA.java
@@ -5,6 +5,9 @@ public class CDA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world");
+		
+		
+		System.out.println("another test");
 	}
 
 }
