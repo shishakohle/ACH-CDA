7a2c6ed9319fcd8a31071fcc320d4040262a8f05
Ingo Weigel <ingo.weigel@myoha.at>
2019-12-13 15:41:54 +0100

    added code to read ten lines from a cda file

--- src/main/java/at/ach/CDA/CDA.java
+++ src/main/java/at/ach/CDA/CDA.java
@@ -1,13 +1,41 @@
 package at.ach.CDA;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class CDA {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args)
+	{
 		System.out.println("hello world");
 		
+		//String cdaDirectory = "./resources/cda/";
 		
-		System.out.println("another test");
+		// This is the improvised path (applying to Dahn's machine)
+		String cdaDirectory = "/*TODO*/";
+		
+		// This is the improvised path (applying to Ingo's machine)
+		// String cdaDirectory = "/home/ingo/git/ACH-CDA/src/main/resources/cda/";
+		
+		File cdafile = new File(cdaDirectory+"labreport0.cda");
+		
+		Scanner sc=null;
+		
+		try
+		{
+			sc = new Scanner(cdafile);
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		
+		for(int i=0; i<10; i++)
+		{
+			String line = sc.nextLine();
+			System.out.println(line);
+		}
 	}
 
 }
