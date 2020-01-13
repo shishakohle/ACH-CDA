c6e94e1b2b990c7053c7c53616dfa13f9c582878
Ingo Weigel <me19m022@technikum-wien.at>
2020-01-13 19:54:38 +0100

    Created the Main class with main() as entry point for the application.

---
+++ src/main/java/at/ach/CDA/Main.java
@@ -0,0 +1,26 @@
+/* An application that juggles with CDA labreports.
+ * 
+ *  Written by Daeun Kim and Ingo Weigel.
+ *  January 14th, 2020
+ *  
+ *  Find this project on GitHub: https://github.com/shishakohle/ACH-CDA
+ *  
+ *  This project was carried out in the "Applications for Crowdsourced Healthcare"
+ *  course at University of Applied Sciences Technikum Wien (Vienna, Austria)
+ *  under the guidance of lecturer Matthias Frohner, PhD, MSc.
+ */
+
+package at.ach.CDA;
+
+/* This "Main" Class contains the main()
+ * to be executed to run the application.
+ */
+public class Main
+{
+	
+	public static void main(String[] args)
+	{
+		
+	}
+
+}
